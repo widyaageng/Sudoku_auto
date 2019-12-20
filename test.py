7dd6792c08c912647cd0b4ffb35049c8448ee89f
Widya_Ageng <ageng.setyatutuko@gmail.com>
2019-12-21 00:48:50 +0800

    changing test array

--- test.py
+++ test.py
@@ -6,13 +6,13 @@ from test_resources import sudoku_test10_gen, sudoku_test10_drop_number
 train_data = sudoku_gen.sudoku_gen_init()
 # sudoku_gen.sudoku_gen_print(train_data)
 train_data = sudoku_batch100_gen.sudoku_batch100_gen_init(train_data)
-print(train_data[:, :, 5])
+print(train_data[:, :, 1])
 
 # Getting 10 batch of test data
 test_data = sudoku_gen.sudoku_gen_init()
 # sudoku_gen.sudoku_gen_print(test_data)
 test_data = sudoku_test10_gen.sudoku_test10_gen_init(test_data)
-print(test_data[:, :, 5])
+print(test_data[:, :, 1])
 test_data = sudoku_test10_drop_number.sudoku_drop_cell(test_data, drop_cell=10)
-print(test_data[:, :, 5])
+print(test_data[:, :, 1])
 
