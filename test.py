5861316a55e792bbbc5b280699998b28f2693021
Widya_Ageng <ageng.setyatutuko@gmail.com>
2019-12-20 23:21:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from util import sudokufy
 from train_resource import sudoku_gen as sgen
 from test_resources import sudoku_test10_drop_number as testdrop10
 import random
+from util import sudoku_flat
 
 # print(fetch.get_html_txt_headless('https://github.com/widyaageng/Sudoku_auto'))
 
@@ -10,3 +11,9 @@ testfield = sgen.sudoku_gen_init()
 print(testfield)
 testfield = testdrop10.sudoku_drop_cell(testfield, 40)
 print(testfield)
+testfield = sudoku_flat.flat_sudoku_to_col_vec(testfield)
+print(testfield)
+print(testfield.shape)
+testfield = sudoku_flat.flat_sudoku_to_row_vec(testfield)
+print(testfield)
+print(testfield.shape)
