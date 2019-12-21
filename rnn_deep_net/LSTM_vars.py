407bec9e8be18db256f72665029474180c449363
Widya_Ageng <ageng.setyatutuko@gmail.com>
2019-12-21 19:09:11 +0800

    vars + sigmoid

--- rnn_deep_net/LSTM_vars.py
+++ rnn_deep_net/LSTM_vars.py
@@ -1,4 +1,11 @@
 import numpy as np
+from keras.layers import Dense, Activation, Dropout, Input, LSTM, Reshape, Lambda, RepeatVector
+from keras.initializers import glorot_uniform
+from keras.utils import to_categorical
+from keras.optimizers import adam
+from keras import backend as K
+
 
 input_tx = 81
 output_tx = 81
+
