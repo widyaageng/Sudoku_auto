63b0aad1b56570d7e7c9639563c8af15e8be6646
Widya_Ageng <ageng.setyatutuko@gmail.com>
2019-12-24 22:53:33 +0800

    LSTM ceel update

--- rnn_deep_net/LSTM_Cell.py
+++ rnn_deep_net/LSTM_Cell.py
@@ -17,6 +17,7 @@ class LSTMCell:
             current_state,
             previous_hidden,
             current_hidden,
+            current_input,
             activated_forget_ft,
             activated_input_it,
             activated_output_ot,
@@ -34,13 +35,35 @@ class LSTMCell:
         self.current_state = current_state
         self.previous_hidden = previous_hidden
         self.current_hidden = current_hidden
+        self.current_input = current_input
         self.activated_forget_ft = activated_forget_ft
         self.activated_input_it = activated_input_it
         self.activated_output_ot = activated_output_ot
 
+
+
+
+    def dim_check(wrapped_function):
+        def check_dim(self):
+            try:
+                assert (np.shape(self.forget_input_weight_wf)[1] == np.shape(self.current_input)[0])
+                assert (np.shape(self.forget_hidden_weight_uf)[1] == np.shape(self.previous_hidden)[0])
+                assert (np.shape(self.forget_bias_bf)[1] == np.shape(self.forget_input_weight_wf)[0])
+                wrapped_function
+            except AssertionError:
+                print('Dimension mismatch in \'forget\' constructor!')
+        return check_dim()
+
+    @dim_check
     def get_forget_weight(self):
         return self.forget_input_weight_wf, self.forget_hidden_weight_uf, self.forget_bias_bf
 
+    @dim_check
+    def get_input_weight(self):
+        return self.input_input_weight_wi, self.input_hidden_weight_ui, self.input_bias_bi
 
-LSTM_unit = LSTMCell(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
-print(LSTM_unit.get_forget_weight()[1])
+    @dim_check
+    def get_output_weight(self):
+        return self.output_input_weight_wo, self.output_hidden_weight_wo, self.output_bias_bo
+
+
